210578236fee3a93f5b8515a8b3a671e85691cec
Schansge <grosser.knuff@googlemail.com>
2014-05-03 15:54:47 +0200

    Show rating prompt in debug mode

--- Festify/SMAppDelegate.m
+++ Festify/SMAppDelegate.m
@@ -56,11 +56,11 @@ static NSString * const kCallbackURL = @"spotify-ios-sdk-beta://callback";
 
     // config appirater rating request system
     // TODO: [Appirater setAppId:@"123456789"];
-    [Appirater setDaysUntilPrompt:7];
-    [Appirater setUsesUntilPrompt:5];
-    [Appirater setSignificantEventsUntilPrompt:-1];
-    [Appirater setTimeBeforeReminding:2];
+#ifdef DEBUG
+    [Appirater setDebug:YES];
+#else
     [Appirater setDebug:NO];
+#endif
     [Appirater appLaunched:YES];
     
     return YES;
